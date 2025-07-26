6c40ef512d31a7d64dbcd8212e378e507601f8d9
Gih <giovannabreinackcolombara@gmail.com>
2025-07-26 17:54:52 -0300

    criação dos components footer e navbar

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+
+    return (
+        <>
+            <div className="flex justify-center bg-teal-600 text-white">
+                <div className="container flex flex-col items-center py-4">
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-5
+            			   bg-teal-600 text-white'>
+
+                <div className="container flex justify-between text-lg">
+                    Desafio Dev
+
+                    <div className='flex gap-4'>
+                        Cadastrar
+                        Atualizar
+                        Excluir
+                        Procurar
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
